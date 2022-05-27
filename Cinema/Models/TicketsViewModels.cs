76a90e41f9a0fe1df40c8a11b0b4a83d00ed4518
M4TT3O-91 <teo_exc@hotmail.it>
2022-05-27 13:01:18 +0200

    add ticket model

--- Cinema/Models/TicketsViewModels.cs
+++ Cinema/Models/TicketsViewModels.cs
@@ -1,6 +1,18 @@
-﻿namespace Cinema.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cinema.Models
 {
     public class TicketsViewModels
     {
+        public int ID { get; set; }
+        [PositiveNotZero(ErrorMessage ="Ticket price must be positive and not 0")]
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+        [Required]
+        public int SpectatorID { get; set; }
+        [Required]
+        public int FilmID { get; set; }
+        public bool IsValid { get; set; }
+
     }
 }
